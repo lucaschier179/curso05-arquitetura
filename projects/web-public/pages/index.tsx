78875d972c580c38f3405bf3b9b0404e545687c9
lucaschier <ens-lucasschier@ugv.edu.br>
2023-05-17 17:18:09 -0300

    Yarn Workspaces e o pacote utils

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import { sum } from "@alura/utils/math/sum";
 
 export default function HomeScreen() {
   return (
     <main>
       <h1>Home</h1>
+      <p>Importando módulo local: @alura/utils/math/sum sum(2,2): {sum(2,2)}</p>
     </main>
   )
 }
