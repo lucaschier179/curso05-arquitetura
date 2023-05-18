039d3517028e37375516969c6c2d836284a306e7
lucaschier <ens-lucasschier@ugv.edu.br>
2023-05-18 14:02:11 -0300

    Adicionando e configurando o ESLint no projeto

--- projects/web-public/pages/_app.tsx
+++ projects/web-public/pages/_app.tsx
@@ -1,5 +1,9 @@
-import type { AppProps } from 'next/app'
-import { ThemeProvider, DefaultTheme, createGlobalStyle } from 'styled-components';
+import type { AppProps } from "next/app";
+import {
+  ThemeProvider,
+  DefaultTheme,
+  createGlobalStyle,
+} from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -7,21 +11,22 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-`
+`;
 
 const theme: DefaultTheme = {
   colors: {
-    primary: '#111',
-    secondary: '#0070f3',
+    primary: "#111",
+    secondary: "#0070f3",
   },
-}
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -5,7 +5,9 @@ export default function HomeScreen() {
   return (
     <main>
       <Text tag="h1">Home</Text>
-      <Text tag="p">Importando módulo local: @alura/utils/math/sum sum(2,2): {sum(2,2)}</Text>
+      <Text tag="p">
+        Importando módulo local: @alura/utils/math/sum sum(2,2): {sum(2, 2)}
+      </Text>
     </main>
-  )
+  );
 }
