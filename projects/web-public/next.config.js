3a57ff33318f71d3f354e940847fcd7608cf96f7
lucaschier <ens-lucasschier@ugv.edu.br>
2023-05-18 08:43:12 -0300

    Instalando o módulo de UI e o next-transpile-modules

---
+++ projects/web-public/next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  transpilePackages: [
+    '@alura/design-system',
+    '@alura/utils'
+  ],
+};
--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,10 +1,11 @@
+import { Text } from "@alura/design-system/components/Text";
 import { sum } from "@alura/utils/math/sum";
 
 export default function HomeScreen() {
   return (
     <main>
-      <h1>Home</h1>
-      <p>Importando módulo local: @alura/utils/math/sum sum(2,2): {sum(2,2)}</p>
+      <Text tag="h1">Home</Text>
+      <Text tag="p">Importando módulo local: @alura/utils/math/sum sum(2,2): {sum(2,2)}</Text>
     </main>
   )
 }
